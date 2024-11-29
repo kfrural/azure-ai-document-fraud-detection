06c97e446521df33d05052b903bd486ba2e33c28
Karla C. Ferreira <karlinharural@gmail.com>
2024-11-29 10:27:04 +0000

    starting-project

---
+++ src/ingestion/upload_documents.py
@@ -0,0 +1,11 @@
+import os
+from azure.storage.blob import BlobServiceClient
+import json
+
+config = {
+    "storage_account_name": "<seu_storage_account>",
+    "storage_account_key": "<sua_storage_key>"
+}
+
+def upload_document(file_path):
+    pass
---
+++ src/processing/analyze_document.py
@@ -0,0 +1,2 @@
+def analyze_document(blob_url):
+    pass
